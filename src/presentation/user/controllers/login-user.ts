a85113ad7975d0eb4e1821d0e5bc33646cb92ee8
Matheus Dias <dias.math0@gmail.com>
2022-11-16 15:48:45 -0300

    feat(user): created presentation layer and user login controller
    Created presentation layer and user login controller

---
+++ src/presentation/user/controllers/login-user.ts
@@ -0,0 +1,26 @@
+import { LoginUserUseCase } from '../../../domain/user';
+import { Controller } from '../../contracts';
+import { HttpResponse } from '../../types';
+
+export class LoginUserController implements Controller {
+  constructor(private loginUserUseCase: LoginUserUseCase) {}
+
+  async handle(request: any): Promise<HttpResponse> {
+    try {
+      const user = await this.loginUserUseCase.execute({
+        email: request.email,
+        password: request.password,
+      });
+
+      return {
+        statusCode: 200,
+        body: user,
+      };
+    } catch (err) {
+      return {
+        statusCode: 400,
+        body: '',
+      };
+    }
+  }
+}
