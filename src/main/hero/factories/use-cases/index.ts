219b5c327e27fd28d949a2553023f12946bc8522
Matheus Dias <dias.math0@gmail.com>
2022-11-19 22:34:22 -0300

    feat(hero): created query in graphql to fetch hero by id

--- src/main/hero/factories/use-cases/index.ts
+++ src/main/hero/factories/use-cases/index.ts
@@ -1,1 +1,2 @@
 export * from './fetch-heroes-use-case-factory';
+export * from './fetch-hero-by-id-use-case-factory';
---
+++ src/presentation/hero/controllers/index.ts
@@ -0,0 +1,2 @@
+export * from './fetch-heroes-controller';
+export * from './fetch-hero-by-id-controller';
