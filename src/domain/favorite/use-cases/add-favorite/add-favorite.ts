f8f9a3904b216c355419e850a56284c9ed970310
Matheus Dias <dias.math0@gmail.com>
2022-11-22 11:20:40 -0300

    test(favorite): changed tests to remove favorite name

--- src/domain/favorite/use-cases/add-favorite/add-favorite.ts
+++ src/domain/favorite/use-cases/add-favorite/add-favorite.ts
@@ -3,26 +3,7 @@ import { AddFavoriteInput, IAddFavoriteRepository } from '../..';
 export class AddFavoriteUseCase {
   constructor(private readonly addFavoriteRepository: IAddFavoriteRepository) {}
 
-  async execute({
-    userId,
-    heroId,
-    favoriteName,
-  }: AddFavoriteInput): Promise<void> {
-    //Check if the favorite name is empty
-    if (favoriteName.trim() === '') {
-      throw new Error();
-    }
-
-    //Check if the favorite name is less than 4 characters
-    if (favoriteName.length < 4) {
-      throw new Error();
-    }
-
-    //Check if the favorite name is longer than 15 characters
-    if (favoriteName.length > 15) {
-      throw new Error();
-    }
-
+  async execute({ userId, heroId }: AddFavoriteInput): Promise<void> {
     //Check if the user id has not been provided
     if (!userId) {
       throw new Error();
@@ -37,7 +18,6 @@ export class AddFavoriteUseCase {
     await this.addFavoriteRepository.addFavorite({
       userId,
       heroId,
-      favoriteName,
     });
   }
 }
