b636412f200072afd6fd0524f61c10d467f816e5
Matheus Dias <dias.math0@gmail.com>
2022-11-23 16:32:33 -0300

    refactor(favorite): favorite repository in memory separated into files
    Files created to add favorite, fetch favorite by id and remove favorite

---
+++ src/infra/favorite/implementation/repositories/in-memory/index.ts
@@ -0,0 +1,3 @@
+export * from './add-favorite-repository-in-memory/add-favorite-repository-in-memory';
+export * from './fetch-favorite-by-id-repository-in-memory/fetch-favorite-by-id-repository-in-memory';
+export * from './remove-favorite-repository-in-memory/remove-favorite-repository-in-memory';
