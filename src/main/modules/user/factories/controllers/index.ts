e48fa620432f26456fe61f997b33b08b11229e91
Matheus Dias <dias.math0@gmail.com>
2022-11-25 17:20:40 -0300

    feat(user): new route created to confirm registration
    Created controller, use case factory and controller and route

--- src/main/modules/user/factories/controllers/index.ts
+++ src/main/modules/user/factories/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './login-user-controller-factory';
 export * from './register-user-controller-factory';
+export * from './confirm-registration-controller';
--- src/main/modules/user/factories/use-cases/index.ts
+++ src/main/modules/user/factories/use-cases/index.ts
@@ -1,2 +1,3 @@
 export * from './login-user-use-case-factory';
 export * from './register-user-use-case-factory';
+export * from './confirm-registration-use-case-factory';
